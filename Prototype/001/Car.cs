e06afe23bc574c7dfedf22ed8f5a432b8572ebb8
Davydenko Roman <ishadows888@gmail.com>
2020-08-28 22:43:10 +0700

    Добавлен пример реализации паттерна Прототип (Prototype) с помощью стандартного интерфейса ICloneable.

---
+++ Prototype/001/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype._001
+{
+	/// <summary>
+	/// Машина, реализующая интерфейс <see cref="ICloneable"/>
+	/// </summary>
+	public class Car : ICloneable
+	{
+		/// <summary>
+		/// Максимальная скорость
+		/// </summary>
+		public int MaxSpeed { get; set; }
+
+		/// <summary>
+		/// Количество топлива
+		/// </summary>
+		public int Oil { get; set; }
+
+		public object Clone()
+		{
+			var car = new Car() { MaxSpeed = this.MaxSpeed, Oil = this.Oil };
+			return car;
+		}
+	}
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prototype._001;
+
+namespace Prototype
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Создаем оригинальный объект
+			var car = new Car() { MaxSpeed = 180, Oil = 60 };
+
+			// Выполняем клонирование объекта
+			var clon = car.Clone() as Car;
+
+			// Изменяем оригинальный
+			car.Oil = 0;
+
+			// Выводим на экран данные об обеих машинах
+			Console.WriteLine($"Оригинальная: Скорость {car.MaxSpeed}. Топливо {car.Oil}");
+			Console.WriteLine($"Копия: Скорость {clon.MaxSpeed}. Топливо {clon.Oil}");
+		}
+	}
+}
